b121c0bc6f7bb0dd6cb7e52ab7b2e947287b05ea
Nguyễn Hồng Đức <contact.hongduc@gmail.com>
2022-12-24 10:38:48 +0700

    Fix Bug

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import useSWR from "swr";
 import { fetcher, api_key } from "../configs/apiConfig";
 import { BsCloudRain, BsSearch } from "react-icons/bs";
+import { useDebounce } from "../hooks/useDebounce";
 
 import Rain from "../assets/images/rain.jpg";
 import Imagebg from "../components/Imagebg";
@@ -15,108 +16,137 @@ const HomePage = () => {
    const [lat, setLat] = React.useState(21.0278);
    const [lon, setLon] = React.useState(105.8342);
 
+   const filterSearch = useDebounce(searchData, 2000);
+
+   // nếu ô search rỗng thì lấy vị trí hiện tại
+
    React.useEffect(() => {
-      navigator.geolocation.getCurrentPosition((position) => {
-         const { latitude, longitude } = position.coords;
-         setLat(latitude);
-         setLon(longitude);
-      });
-   }, []);
+      if (searchData == "") {
+         navigator.geolocation.getCurrentPosition((position) => {
+            const { latitude, longitude } = position.coords;
+            setLat(latitude);
+            setLon(longitude);
+         });
+      }
+   }, [searchData]);
 
    const time = new Date();
 
-   const { data, error } = useSWR(
-      `https://api.openweathermap.org/data/2.5/weather?q=${search}&lat=${lat}&lon=${lon}&appid=${api_key}&units=metric&lang=vi`,
+   const { data, error, isLoading } = useSWR(
+      `https://api.openweathermap.org/data/2.5/weather?q=${filterSearch}&lat=${lat}&lon=${lon}&appid=${api_key}&units=metric&lang=vi`,
       fetcher
    );
 
-   if (error) console.log(error);
-
+   // lấy dữ liệu từ ô search
    const handlerSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
       setSearchData(e.target.value);
    };
 
-   const handlerClickButton = () => {
-      setSearch(searchData);
-   };
+
 
    return (
-      <div className="w-screen h-screen relative">
-         {/* nếu mà không có data và xảy ra lỗi thì hiển thị Clear */}
-         <Imagebg weather={data?.weather[0]?.main || "Clear"} />
-
-         <div className="absolute left-[10vw] bottom-[10vh] flex items-center gap-4">
-            {!data ? (
-               <div className="text-white text-2xl">
-                  Không tìm thấy thành phố
-               </div>
-            ) : (
-               <>
-                  <h1 className="text-8xl text-white font-semibold">
-                     {Math.round(data?.main?.temp)}
-                     <span className="align-top text-4xl">o</span>
-                  </h1>
-                  <div className="text-white">
-                     <h2 className="font-medium text-5xl">{data?.name}</h2>
-                     <h4 className="text-sm">
-                        {/* 06:00 PM - Sunday, 5 Oct 22 */}
-                        {time.toLocaleTimeString()} - {time.toDateString()}
-                     </h4>
+      <div className="w-screen h-screen relative bg-slate-800">
+         {isLoading ? (
+            <div className="w-full h-full flex justify-center items-center">
+               <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-white"></div>
+            </div>
+         ) : (
+            <>
+               <a href="/" className="">
+                  <div className="absolute top-[10vh] left-[10vw] flex items-center gap-2">
+                     <BsCloudRain className="text-4xl text-white" />
+                     <h1 className="text-white text-2xl font-semibold">
+                        Weather App
+                     </h1>
                   </div>
-                  <div className="text-white">
-                     <WeatherIcon weather={data?.weather[0]?.main} />
-                     <span className="font-medium">
-                        {data?.weather[0]?.main}
-                     </span>
-                  </div>
-               </>
-            )}
-         </div>
+               </a>
 
-         <div className="absolute top-0 right-0 w-[30vw] h-screen glass-effect">
-            <div className="flex items-center h-[70px]">
-               <div className="px-6 search-input h-full">
-                  <input
-                     type="text"
-                     className="h-full w-full bg-transparent border-b-2 text-white placeholder:text-white outline-none"
-                     placeholder="Anothor weather"
-                     onChange={handlerSearch}
+               {data?.cod == "404" ? (
+                  <img
+                     src={Rain}
+                     alt="weather"
+                     className="w-full h-full bg-cover pointer-events-none bg-no-repeat bg-center"
                   />
-               </div>
-               <button
-                  className="bg-slate-500 h-full w-[70px] flex items-center justify-center"
-                  onClick={handlerClickButton}>
-                  <BsSearch className="text-2xl text-white" />
-               </button>
-            </div>
+               ) : (
+                  <Imagebg weather={data ? data?.weather[0]?.main : "Clear"} />
+               )}
 
-            <div className="p-6">
-               <div className="text-white border-b-2 py-6">
-                  <h2 className="font-semibold">Weather Details</h2>
-                  <div className="flex flex-col gap-5 mt-5">
-                     <div className="flex items-center justify-between">
-                        <span className="">Cloudy</span>
-                        <span className="">
-                           {!data ? 0 : data?.clouds?.all}%
-                        </span>
+               <div className="absolute left-[10vw] bottom-[10vh] flex items-center gap-4">
+                  {data?.cod == "404" ? (
+                     <div className="text-white text-2xl">
+                        Không tìm thấy thành phố
                      </div>
-                     <div className="flex items-center justify-between">
-                        <span className="">Humidity</span>
-                        <span className="">
-                           {!data ? 0 : data?.main?.humidity}%
-                        </span>
+                  ) : (
+                     <>
+                        <h1 className="text-8xl text-white font-semibold">
+                           {Math.round(data?.main?.temp)}
+                           <span className="align-top text-4xl">o</span>
+                        </h1>
+                        <div className="text-white">
+                           <h2 className="font-medium text-5xl">
+                              {data?.name}
+                           </h2>
+                           <h4 className="text-sm">
+                              {/* 06:00 PM - Sunday, 5 Oct 22 */}
+                              {time.toLocaleTimeString()} -{" "}
+                              {time.toDateString()}
+                           </h4>
+                        </div>
+                        <div className="text-white">
+                           <WeatherIcon
+                              weather={data ? data?.weather[0]?.main : "Clear"}
+                           />
+                           <span className="font-medium">
+                              {data ? data?.weather[0]?.main : "Clear"}
+                           </span>
+                        </div>
+                     </>
+                  )}
+               </div>
+
+               <div className="absolute top-0 right-0 w-[30vw] h-screen glass-effect">
+                  <div className="flex items-center h-[70px]">
+                     <div className="px-6 search-input h-full">
+                        <input
+                           type="text"
+                           className="h-full w-full bg-transparent border-b-2 text-white placeholder:text-white outline-none"
+                           placeholder="Anothor weather"
+                           onChange={handlerSearch}
+                        />
                      </div>
-                     <div className="flex items-center justify-between">
-                        <span className="">Wind</span>
-                        <span className="">
-                           {!data ? 0 : data?.wind?.speed} km/h
-                        </span>
+                     <button
+                        className="bg-slate-500 h-full w-[70px] flex items-center justify-center">
+                        <BsSearch className="text-2xl text-white" />
+                     </button>
+                  </div>
+
+                  <div className="p-6">
+                     <div className="text-white border-b-2 py-6">
+                        <h2 className="font-semibold">Weather Details</h2>
+                        <div className="flex flex-col gap-5 mt-5">
+                           <div className="flex items-center justify-between">
+                              <span className="">Cloudy</span>
+                              <span className="">
+                                 {!data ? 0 : data?.clouds?.all}%
+                              </span>
+                           </div>
+                           <div className="flex items-center justify-between">
+                              <span className="">Humidity</span>
+                              <span className="">
+                                 {!data ? 0 : data?.main?.humidity}%
+                              </span>
+                           </div>
+                           <div className="flex items-center justify-between">
+                              <span className="">Wind</span>
+                              <span className="">
+                                 {!data ? 0 : data?.wind?.speed} km/h
+                              </span>
+                           </div>
+                        </div>
                      </div>
                   </div>
-               </div>
-            </div>
 
-            {/* <div className="p-6">
+                  {/* <div className="p-6">
                <div className="text-white border-b-2 py-6">
                   <h2 className="font-semibold">Next Days</h2>
                   <div className="flex flex-col gap-5 mt-5">
@@ -132,7 +162,9 @@ const HomePage = () => {
                   </div>
                </div>
             </div> */}
-         </div>
+               </div>
+            </>
+         )}
       </div>
    );
 };
